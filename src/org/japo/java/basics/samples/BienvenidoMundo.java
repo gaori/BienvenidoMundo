6cfe17562f57a3523232be1ef92b34711201b844
gaori <gaori.chan@gmail.com>
2015-10-14 12:29:17 +0200

    Initial Version

---
+++ src/org/japo/java/basics/samples/BienvenidoMundo.java
@@ -0,0 +1,10 @@
+package org.japo.java.basics.samples;
+
+public class BienvenidoMundo {
+    
+    public static void main(String[] args) {
+        
+         System.out.println("Bienvenido bocata sañldk!");
+    }
+    
+}
